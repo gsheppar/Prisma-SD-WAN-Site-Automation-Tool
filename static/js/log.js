dc23b635e4944326ad5effff0cee9e84c8fbc56b
gsheppar <gsheppar@gmail.com>
2023-07-14 21:15:11 -0400

    Delete static directory

--- static/js/log.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  var output = document.getElementById('output');
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '{{ url_for('log_stream') }}', true);
-  xhr.send();
-  setInterval(function() {
-    output.textContent = xhr.responseText;
-  }, 500);
-});
